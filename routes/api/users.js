aa5011f52d40c81939e93e49f2bfeeae5d1898ae
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-15 11:53:34 +0900

    User model created, Registration Route added, Usser added to MongoDB

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+// User model
+const User = require("../../Models/User");
 
 // @route GET api/users/test
 // @desc Tests users route
 // @access Public
 router.get("/test", (req, res) => {
   res.json({ msg: "Users works" });
+});
+
+// @route GET api/users/register
+// @desc Register new user
+// @access Public
+router.post("/register", (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: "email already exists" });
+    } else {
+      const avatar = gravatar.url(req.body.email, {
+        s: "200", // Size
+        r: "pg", // Rating
+        d: "mm" // Default
+      });
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        // Documentation https://github.com/emerleite/node-gravatar
+        avatar: avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
 });
 
 module.exports = router;
