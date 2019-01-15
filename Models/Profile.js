e60fbfc00256235162114f6ecdba9631359fa200
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-15 16:34:44 +0900

    Profile Schema added

---
+++ Models/Profile.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+// Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  sex: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  social: {
+    twitter: {
+      type: String
+    },
+    instagram: {
+      type: String
+    },
+    facebook: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
