9739e8cdfd7e66de4c6ce661c0dd5f64253b6600
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-15 21:31:09 +0900

    POST route to create new Movie added. Movie validation added

--- routes/api/movies.js
+++ routes/api/movies.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Movie model
+const Movie = require("../../Models/Movie");
+
+// Vaidation
+const validateMovieInput = require("../../validation/movie");
 
 // @route GET api/movies/test
 // @desc Tests movies route
@@ -7,5 +15,30 @@ const router = express.Router();
 router.get("/test", (req, res) => {
   res.json({ msg: "Movies works" });
 });
+
+// @route POST api/movies/
+// @desc Create movie
+// @access Private
+
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateMovieInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newMovie = new Movie({
+      title: req.body.title,
+      overview: req.body.overview,
+      release: req.body.release,
+      comment: req.body.comment,
+      user: req.user.id
+    });
+    newMovie.save().then(movie => res.json(movie));
+  }
+);
 
 module.exports = router;
