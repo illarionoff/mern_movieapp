f405ef4279a93a26bcc5811565f527ed689afd0b
Alex Illarionov <illarionoff.a@gmail.com>
2019-01-16 14:11:32 +0900

    Register and Login components added

--- client/src/components/layout/auth/Login.js
+++ client/src/components/layout/auth/Login.js
@@ -1,26 +1,48 @@
 import React, { Component } from "react";
 
 class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(user);
+  };
+
   render() {
     return (
-      <form className="mx-auto col-md-6 mt-5">
+      <form className="mx-auto col-md-6 mt-5" onSubmit={this.onSubmit}>
         <h2>Login</h2>
         <div className="form-group">
-          <label for="exampleInputEmail1">Email address</label>
+          <label>Email address</label>
           <input
             type="email"
             className="form-control "
-            name="name"
+            name="email"
             placeholder="Enter email"
+            onChange={this.onChange}
           />
         </div>
         <div className="form-group">
-          <label for="exampleInputPassword1">Password</label>
+          <label>Password</label>
           <input
             type="password"
             className="form-control"
             name="password"
             placeholder="Password"
+            onChange={this.onChange}
           />
         </div>
 
